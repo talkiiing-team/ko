9b5388f95ca0f71ff1fcc7a97aad3c14672193c6
roamiiing <unf.hwm@ya.ru>
2021-05-01 17:48:42 +0300

    Fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      safelist: {
+        greedy: [/size-.*$/],
+      },
+    },
+  },
+  darkMode: false,
   theme: {
     container: {
       center: true,
