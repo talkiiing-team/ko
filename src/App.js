fd0674ee686536bb8c915574dff19206c84c16bb
roamiiing <unf.hwm@ya.ru>
2021-05-01 14:59:42 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './scss/main.scss'
 import { Provider } from 'react-redux'
 import { createReduxStore } from './store'
 import Routes from './routes'
+import { Footer } from './components/Footer'
 const store = createReduxStore()
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
+    <>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+      <Footer />
+    </>
   )
 }
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Credits from '../../assets/img/logo.png'
 
 export const Footer = () => (
-  <footer className="w-full flex items-center justify-center">
+  <footer className="w-full my-24 flex items-center justify-center">
     <img src={Credits} />
   </footer>
 )
