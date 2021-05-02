952409f6ebd643c9407b50c0aac9848c30951968
roamiiing <unf.hwm@ya.ru>
2021-05-02 10:38:00 +0300

    Fix auth form

--- src/pages/Main/components/Header/Header.js
+++ src/pages/Main/components/Header/Header.js
@@ -16,19 +16,16 @@ export const Header = ({
   avatar,
   profile,
   setNicknameFunc,
+  onlyLogo,
 }) => {
-    const [showShadow, setShowShadow] = useState(false)
-
-    useEffect(() => {
-      window.addEventListener('scroll', () => {
-        setShowShadow(window.scrollY > 30)
-      })
-    }, [])
-
   return (
     <div
       className={classNames(
-        'fixed bg-white border-b-2 border-gray-100 transition-all duration-200 opacity-90 backdrop-filter backdrop-blur-lg z-50 top-0 left-0 w-full h-32 flex justify-between gap-x-4 items-center p-6',
+        'fixed bg-white border-b-2 border-gray-100 transition-all duration-200 opacity-90 backdrop-filter backdrop-blur-lg z-50 top-0 left-0 w-full h-32 flex gap-x-4 items-center p-6',
+        {
+          'justify-between': !onlyLogo,
+          'justify-center': onlyLogo,
+        }
       )}
     >
       <a
@@ -40,53 +37,54 @@ export const Header = ({
       >
         <img className="w-auto max-w-xl h-16" alt="logo" src={Logo} />
       </a>
-      {!profile ? (
-        <div>
-          <div
-            className="grid grid-flow-col"
-            onClick={() => {
-              if (
-                searchType !== 'ConnectRandom' &&
-                searchType !== 'ConnectCode'
-              ) {
-                history.push(PROFILE_URL)
-                setSearchType('')
-              }
-            }}
-          >
-            <div className="flex items-center">
-              <div className="grid grid-flow-row gap-y-1 text-right">
-                <div className="font-bold text-4xl">{nickname || ''}</div>
-                <div>{pts || 0}pts</div>
-                <div>{winrate || ''}</div>
+      {!onlyLogo &&
+        (!profile ? (
+          <div>
+            <div
+              className="grid grid-flow-col"
+              onClick={() => {
+                if (
+                  searchType !== 'ConnectRandom' &&
+                  searchType !== 'ConnectCode'
+                ) {
+                  history.push(PROFILE_URL)
+                  setSearchType('')
+                }
+              }}
+            >
+              <div className="flex items-center">
+                <div className="grid grid-flow-row gap-y-1 text-right">
+                  <div className="font-bold text-4xl">{nickname || ''}</div>
+                  <div>{pts || 0}pts</div>
+                  <div>{winrate || ''}</div>
+                </div>
               </div>
+              <img
+                className="ml-3 w-24 h-24 rounded-full"
+                alt="avatar"
+                src={avatar}
+              />
             </div>
-            <img
-              className="ml-3 w-24 h-24 rounded-full"
-              alt="avatar"
-              src={avatar}
-            />
           </div>
-        </div>
-      ) : (
-        <>
-          <Input
-            className="max-w-3xl self-center"
-            onChange={(e) => setNicknameFunc(e)}
-            textAlign="left"
-            placeholder="Введите ник или номер игрока"
-          />
-          <ButtonCustom
-            className="w-12 h-12 flex-shrink-0"
-            onClick={() => {
-              history.push(MAIN_URL)
-              setSearchType('')
-            }}
-          >
-            <HomeIcon className="w-8 h-8" />
-          </ButtonCustom>
-        </>
-      )}
+        ) : (
+          <>
+            <Input
+              className="max-w-3xl self-center"
+              onChange={(e) => setNicknameFunc(e)}
+              textAlign="left"
+              placeholder="Введите ник или номер игрока"
+            />
+            <ButtonCustom
+              className="w-12 h-12 flex-shrink-0"
+              onClick={() => {
+                history.push(MAIN_URL)
+                setSearchType('')
+              }}
+            >
+              <HomeIcon className="w-8 h-8" />
+            </ButtonCustom>
+          </>
+        ))}
     </div>
   )
 }
